e168b72f847d55a7d9a32e652129de89cd8d1cb1
Geovanni Berdugo <geovanni.berdugo@gmail.com>
2020-05-24 17:20:36 -0500

    Adding password confirmation and use clean method to validate.

--- estore/forms.py
+++ estore/forms.py
@@ -20,17 +20,27 @@ class RegisterForm(forms.Form):
                                     'class': 'form-control',
                                     'id': 'password',
                                 }))
+    password2 = forms.CharField(required=True, 
+                                label='Password Confirmation', 
+                                widget=forms.PasswordInput(attrs={
+                                    'class':'form-control'
+                                }))
 
     def clean_username(self):
         username = self.cleaned_data.get('username')
         if User.objects.filter(username = username).exists():
-            raise forms.ValidationError('El username ya se encuentra en uso')
+            raise forms.ValidationError('username exists in database, please use other')
 
         return username
 
     def clean_email(self):
         email = self.cleaned_data.get('email')
         if User.objects.filter(email = email).exists():
-            raise forms.ValidationError('El Correo ya se encuentra en uso')
+            raise forms.ValidationError('email exists in database, please use other')
 
-        return username
+        return email
+
+    def clean(self):
+        cleaned_data = super().clean()  #Get method clean from parent class Form
+        if cleaned_data.get('password2') != cleaned_data.get('password'): #Note that password2 depends of password
+            self.add_error('password2', 'Password doesnt match') #Adding error to field password2
