4a6f3aca081fa7c377b4382b38408f16983c350b
Geovanni Berdugo <geovanni.berdugo@gmail.com>
2020-05-25 18:09:18 -0500

    Adding image fiel into Product model

--- estore/urls.py
+++ estore/urls.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import include
 
+from django.conf.urls.static import static
+from django.conf import settings
+
 #App imports
 from . import views
 from products.views import ProductListView
@@ -14,3 +17,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('products/', include('products.urls'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
