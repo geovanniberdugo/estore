3031bb98cf856bfaf6df820577426394189fdcb7
Geovanni Berdugo <geovanni.berdugo@gmail.com>
2020-05-31 08:15:02 -0500

    Adding app_name to producs project in urls.py

--- products/views.py
+++ products/views.py
@@ -3,6 +3,7 @@ from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 
 from .models import Product
+from django.db.models import Q #Multiple Filters
 
 
 class ProductListView(ListView):
@@ -25,7 +26,8 @@ class ProductSearchListView(ListView):
     template_name = 'products/search.html'
 
     def get_queryset(self):
-        return Product.objects.filter(title__icontains=self.query())
+        filters = Q(title__icontains=self.query()) | Q(category__title__icontains=self.query())
+        return Product.objects.filter(filters)
 
     def query(self):
         return self.request.GET.get('q')
