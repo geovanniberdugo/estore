dd1f1f38f85c96bb1f6c8dd65cd8471330d5db95
Geovanni Berdugo <geovanni.berdugo@gmail.com>
2020-05-31 19:08:52 -0500

    Testing session in cart view

--- carts/views.py
+++ carts/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
+from .models import Cart
 
 def cart(request):
+    user = request.user if request.user.is_authenticated else None
+    cart_id = request.session.get('cart_id')
+    
+    if cart_id:
+        #Get the cart from database
+        cart = Cart.objects.get(pk=cart_id
+    else:
+        #Create the cart from database
+        cart = Cart.objects.create(user=user)
+
+    request.session['cart_id'] = cart.id
+
     return render(request, 'carts/cart.html', {
-        
+
     })
